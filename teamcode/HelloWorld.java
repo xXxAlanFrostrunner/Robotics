b4457d6ab1152943ded544ae8dd3305410a2c64a
xXxAlanFrostrunner <106028067+xXxAlanFrostrunner@users.noreply.github.com>
2023-11-24 07:51:23 -0800

    Add files via upload

--- teamcode/HelloWorld.java
+++ teamcode/HelloWorld.java
@@ -6,6 +6,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class HelloWorld extends LinearOpMode {
     @Override
     public void runOpMode() {
-
+    telemetry.addData("Testing","Value");
     }
 }
